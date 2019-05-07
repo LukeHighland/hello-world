b34858a67b7638ac41e45206ca3d451ad26f7615
luke <luke.highland@pcc.edu>
2019-05-06 23:34:53 -0700

    Working helloWorld function

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
 
-    public void HelloWorld() {
-        System.out.println("Hello world!")
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
     }
 }
